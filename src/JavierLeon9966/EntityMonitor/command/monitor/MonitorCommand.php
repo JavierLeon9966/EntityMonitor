15d67c08b17a3a2201e128d4aed4a314b91e6f41
Javier León <58715544+JavierLeon9966@users.noreply.github.com>
2023-07-18 00:12:15 -0300

    Update protocol 594

--- src/JavierLeon9966/EntityMonitor/command/monitor/MonitorCommand.php
+++ src/JavierLeon9966/EntityMonitor/command/monitor/MonitorCommand.php
@@ -8,7 +8,6 @@ use CortexPE\Commando\BaseCommand;
 
 use JavierLeon9966\EntityMonitor\command\monitor\subcommand\{EntitySubCommand, TileSubCommand};
 
-use JavierLeon9966\EntityMonitor\EntityMonitor;
 use pocketmine\command\CommandSender;
 
 class MonitorCommand extends BaseCommand{
@@ -19,9 +18,7 @@ class MonitorCommand extends BaseCommand{
 
 	protected function prepare(): void{
 		$this->setPermissions(['entitymonitor.command.monitor.entity', 'entitymonitor.command.monitor.tile']);
-		$plugin = $this->getOwningPlugin();
-		assert($plugin instanceof EntityMonitor);
-		$this->registerSubCommand(new EntitySubCommand($plugin, 'entity'));
-		$this->registerSubCommand(new TileSubCommand($plugin, 'tile'));
+		$this->registerSubCommand(new EntitySubCommand($this->plugin, 'entity'));
+		$this->registerSubCommand(new TileSubCommand($this->plugin, 'tile'));
 	}
 }
